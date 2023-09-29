cf9dfdfb91f4400541ad5177c274e11bb694087f
John <kiokoj81@gmail.com>
2023-09-29 19:20:10 +0300

    Add routes for the products controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'products/index'
+  get 'products/show'
+  get 'products/new'
+  get 'products/edit'
+  get 'products/search'
+  get 'products/results'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
