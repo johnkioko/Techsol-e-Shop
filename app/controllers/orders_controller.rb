6de8b3163ab61a7ec63e7747336040ab818d15d2
John <kiokoj81@gmail.com>
2023-10-01 20:36:21 +0300

    Create Orders controller

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,19 @@
+class OrdersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def checkout
+  end
+
+  def confirm
+  end
+end
