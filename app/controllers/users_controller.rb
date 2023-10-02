169fe608f1d5f1cc394c282818e9b7fcbfed12d1
John <kiokoj81@gmail.com>
2023-10-02 22:40:28 +0300

    Implement Users controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,18 @@
 class UsersController < ApplicationController
+  
+  # Display registration form for creating a new user account.
   def new
+    @user = User.new
   end
 
+  # Display form for editing user account details (for logged-in users).
   def edit
+    @user = current_user # Load the logged-in user
   end
 
+  # Show user profile and order history (for logged-in users).
   def show
+    @user = current_user # Load the logged-in user
+    @orders = @user.orders # Retrieve user's orders
   end
 end
