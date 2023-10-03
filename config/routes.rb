b63a1b6ae495802fd40e75b317c383f21b203dae
John <kiokoj81@gmail.com>
2023-10-03 21:39:55 +0300

    Update product controller routes

--- config/routes.rb
+++ config/routes.rb
@@ -31,12 +31,7 @@ Rails.application.routes.draw do
   get 'categories/show'
   get 'categories/new'
   get 'categories/edit'
-  get 'products/index'
-  get 'products/show'
-  get 'products/new'
-  get 'products/edit'
-  get 'products/search'
-  get 'products/results'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
