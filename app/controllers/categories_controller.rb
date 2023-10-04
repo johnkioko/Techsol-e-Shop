47d6ed91f8474c6354bef415a3262259ded32488
John <kiokoj81@gmail.com>
2023-10-04 23:26:27 +0300

    Implement Categories controller actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,26 @@
 class CategoriesController < ApplicationController
+  # List of all product categories.
   def index
+    @categories = Category.all
   end
 
-  def show
-  end
-
+  # Form for creating a new category.
   def new
+    @category = Category.new
   end
 
+  # Form for editing an existing category.
   def edit
+    @category = Category.find(params[:id])
+  end
+
+  # Detailed view of a category.
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  # Confirmation page for deleting a category.
+  def destroy
+    @category = Category.find(params[:id])
   end
 end
