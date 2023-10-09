79edbd92d5a4cca9514c7999765581a32d94a9b9
John <kiokoj81@gmail.com>
2023-10-09 20:51:26 +0300

    Implement ShipmentsController actions for showing and editing shipments

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -1,7 +1,13 @@
 class ShipmentsController < ApplicationController
+  before_action :authenticate_admin!, only: [:edit]
+  
+  # Display shipment details and tracking information.
   def show
+    @shipment = Shipment.find(params[:id])
   end
 
+  # Display a form for editing shipment details (for admin users).
   def edit
+    @shipment = Shipment.find(params[:id])
   end
 end
