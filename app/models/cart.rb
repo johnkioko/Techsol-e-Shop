775f315a9834a132879da8f3af4f13a6ceac98d1
John <kiokoj81@gmail.com>
2023-10-09 15:46:37 +0300

    Add add_product method to Cart model for managing cart items

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,6 +4,22 @@ class Cart < ApplicationRecord
 
   belongs_to :user
   belongs_to :product
+  has_many :cart_items
+
+  # Add a product to the cart.
+  def add_product(product)
+    # Check if the product is already in the cart.
+    existing_item = cart_items.find_by(product_id: product.id)
+
+    if existing_item
+      # If the product is already in the cart, increment its quantity.
+      existing_item.quantity += 1
+      existing_item.save
+    else
+      # If the product is not in the cart, create a new cart item.
+      cart_items.create(product: product, quantity: 1)
+    end
+  end
 
   # Validations: Add validations as needed for your cart-related attributes.
   validates :user_id, :product_id, :quantity, presence: true
