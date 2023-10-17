b115fc3e94e71701cae58cab3349caea94cd7a75
John <kiokoj81@gmail.com>
2023-10-17 09:04:09 +0300

    Add associations for Admin model

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,2 +1,6 @@
 class Admin < ApplicationRecord
+    has_many :products, class_name: 'Product', foreign_key: 'admin_id'
+    has_many :categories, class_name: 'Category', foreign_key: 'admin_id'
+
+    
 end
