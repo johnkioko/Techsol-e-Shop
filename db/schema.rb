b12b8dd3eb9eb4af9b35d83ffb8d46b426fb487e
John <kiokoj81@gmail.com>
2023-10-17 08:57:05 +0300

    Run migrations for Admin Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_17_052019) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_17_055345) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_17_052019) do
     t.string "state"
     t.string "postal_code"
     t.string "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "admins", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
