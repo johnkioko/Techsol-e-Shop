b1222bf2cae7bd9556ba0cf630bdefaebcfbe670
John <kiokoj81@gmail.com>
2023-10-27 15:28:20 +0300

    Update price attribute on products migration

--- db/migrate/20230928084717_create_products.rb
+++ db/migrate/20230928084717_create_products.rb
@@ -3,7 +3,7 @@ class CreateProducts < ActiveRecord::Migration[7.0]
     create_table :products do |t|
       t.string :name
       t.text :description
-      t.decimal :price
+      t.decimal :price, :precision => 8, :scale => 2
       t.integer :quantity_in_stock
       # t.references :category, null: false, foreign_key: true
       t.string :image_url
