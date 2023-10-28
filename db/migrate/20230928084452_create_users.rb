1a99dc899f6a8d7c4e9defa98e4eb7c8c0abecf9
John <kiokoj81@gmail.com>
2023-10-28 23:35:23 +0300

    Run database migrations

--- db/migrate/20230928084452_create_users.rb
+++ db/migrate/20230928084452_create_users.rb
@@ -5,11 +5,11 @@ class CreateUsers < ActiveRecord::Migration[7.0]
       t.string :password_digest
       t.string :first_name
       t.string :last_name
-      t.references :address, null: false, foreign_key: true
+      # t.references :address, null: false, foreign_key: true
 
       t.timestamps
     end
-    add_index :users, :email
+    # add_index :users, :email
   end
 end
 
--- db/migrate/20230928084717_create_products.rb
+++ db/migrate/20230928084717_create_products.rb
@@ -3,12 +3,14 @@ class CreateProducts < ActiveRecord::Migration[7.0]
     create_table :products do |t|
       t.string :name
       t.text :description
-      t.decimal :price, :precision => 8, :scale => 2
-      t.integer :quantity_in_stock
-      # t.references :category, null: false, foreign_key: true
       t.string :image_url
+      t.decimal :price, :precision => 8, :scale => 2
+
 
       t.timestamps
     end
   end
 end
+
+      # t.integer :quantity_in_stock
+      # t.references :category, null: false, foreign_key: true
