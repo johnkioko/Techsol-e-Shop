5e9457a13c41c6dbb284426c4b6435b1065021e3
John <kiokoj81@gmail.com>
2023-10-29 22:24:06 +0300

    Clean up product model to remove duplicate validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,8 @@ class Product < ApplicationRecord
   has_many :reviews
   has_many :order_items
 
-  validates :name, :description, :price, :image_url, presence: true
-  validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validates :name, :description, :image_url, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 end
 
 # :quantity_in_stock,  :category_id,
