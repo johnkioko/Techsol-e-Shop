7e699809b818e7d1930767be31e678d98409b044
John <kiokoj81@gmail.com>
2023-10-30 11:29:42 +0300

    Add image validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,12 @@ class Product < ApplicationRecord
   has_many :order_items
 
   validates :name, :description, :image_url, presence: true
-  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :price, numericality: { greater_than_or_equal_to: 0.00 }
+  validates :image_url, :format => {
+    :with => %r{\.(gif|jpg|png)$}i,
+    :message => 'must be a URL for GIF, JPG or PNG image.'
+  }
+
 end
 
 # :quantity_in_stock,  :category_id,
