7fcd76cbb502f5b53d4e48cbc4d1e16fede06399
John <kiokoj81@gmail.com>
2023-10-30 11:30:46 +0300

    Point root route to products index

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "welcome#index"
+  root "products#index"
 
   resources :users, only: [:new, :create, :edit, :update, :show, :index]
   resources :products
