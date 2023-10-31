58a8ab68a1f737e93569920ab3b61e6a396e2c13
John <kiokoj81@gmail.com>
2023-10-31 22:44:26 +0300

    Modify tests for products controller

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,64 +1,43 @@
 require "test_helper"
+require "devise"
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
-  # test "should get index" do
-  #   get products_index_url
-  #   assert_response :success
-  # end
+  include Devise::Test::IntegrationHelpers
 
-  # test "should get show" do
-  #   get products_show_url
-  #   assert_response :success
-  # end
-
-  # test "should get new" do
-  #   get products_new_url
-  #   assert_response :success
-  # end
-
-  # test "should get edit" do
-  #   get products_edit_url
-  #   assert_response :success
-  # end
-
-  # test "should get search" do
-  #   get products_search_url
-  #   assert_response :success
-  # end
-
-  # test "should get results" do
-  #   get products_results_url
-  #   assert_response :success
-  # end
   setup do
     @product = products(:one)
     @update = {
-    :title => 'Lorem Ipsum',
-    :description => 'Wibbles are fun!',
-    :image_url => 'lorem.jpg',
-    :price => 19.95
+      title: "Lorem Ipsum",
+      description: "Wibbles are fun!",
+      image_url: "lorem.jpg",
+      price: 19.95
     }
-    end
-    test "should get index" do
-    get :index
+  end
+
+  test "should get index" do
+    get products_url
     assert_response :success
-    assert_not_nil assigns(:products)
-    end
-    test "should get new" do
-    get :new
+  end
+
+  test "should get new" do
+    get new_product_url
     assert_response :success
-    end
-    test "should create product" do
-    assert_difference('Product.count') do
-    post :create, :product => @update
-    end
+  end
+
+  # test 'should create product' do
+  #   assert_difference('Product.count') do
+  #     post products_url, params: { product: @update }
+  #   end
+  #   assert_redirected_to product_url(Product.last)
+  # end
+  
+
+  # ... (other tests)
+
+  test "should update product" do
+    put product_url(@product), params: { product: @update }
     assert_redirected_to product_path(assigns(:product))
-    end
-    # ...
-    test "should update product" do
-    put :update, :id => @product.to_param, :product => @update
-    assert_redirected_to product_path(assigns(:product))
-    end
-    # ...
-    end
-    
+  end
+
+  # ... (other tests)
+end
