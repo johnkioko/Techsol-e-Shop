a519dbfec39ced8394ec6f4561c19dcfdc81da8b
John <kiokoj81@gmail.com>
2023-11-03 14:28:22 +0300

    Configure store/index as the root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  get 'store/index'
+  root 'store/index', as: 'store_index'
   resources :carts
   devise_for :users
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "products#index"
+  # root "products#index"
 
   resources :users, only: [:new, :create, :edit, :update, :show, :index]
   resources :products
