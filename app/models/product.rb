ab986d71c2fb9fcca63516555c075c11d5a89c96
John <kiokoj81@gmail.com>
2023-11-05 08:45:40 +0300

    Add declaration to product and a hook to prevent it from being deleted when it has associated line items

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,19 +1,26 @@
 class Product < ApplicationRecord
   has_many :reviews
   has_many :order_items
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
 
   validates :name, :description, :image_url, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.00 }
-  # validates :image_url, :format => {
-  #   :with => %r{\.(gif|jpg|png)$}i,
-  #   :message => 'must be a URL for GIF, JPG or PNG image.'
-  # }
 
   validates :image_url, format: {
-  with: /\A.*\.(gif|jpg|png)\z/i,
+  with: /\A.*\.(gif|jpg|jpeg|png)\z/i,
   message: 'must be a URL for GIF, JPG or PNG image.',
   multiline: true
 }
+
+private
+ # ensure that there are no line items referencing this product
+ def ensure_not_referenced_by_any_line_item
+  unless line_items.empty?
+    errors.add(:base, 'Line Items present')
+    throw :abort
+  end
+ end
 
 end
 
