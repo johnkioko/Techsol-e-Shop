62327d2ba6918ca78320f264cd0b3af2d25d5a06
John <kiokoj81@gmail.com>
2023-11-18 08:45:07 +0300

    Create orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,33 +1,70 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user! # Ensure user is logged in
+  before_action :set_order, only: %i[ show edit update destroy ]
 
-  # List user's order history (for logged-in users).
+  # GET /orders or /orders.json
   def index
-    @orders = current_user.orders
+    @orders = Order.all
   end
 
-  # Display details of a specific order.
+  # GET /orders/1 or /orders/1.json
   def show
-    @order = Order.find(params[:id])
   end
 
-  # Display a form for creating a new order (when adding products to the cart).
+  # GET /orders/new
   def new
-    # Implement logic for creating a new order here
+    @order = Order.new
   end
 
-  # Confirmation page after order placement.
+  # GET /orders/1/edit
+  def edit
+  end
+
+  # POST /orders or /orders.json
   def create
-    # Implement logic for order creation here
+    @order = Order.new(order_params)
+
+    respond_to do |format|
+      if @order.save
+        format.html { redirect_to order_url(@order), notice: "Order was successfully created." }
+        format.json { render :show, status: :created, location: @order }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @order.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
-  # Display order details and payment form.
-  def checkout
-    # Implement logic for order checkout here
+  # PATCH/PUT /orders/1 or /orders/1.json
+  def update
+    respond_to do |format|
+      if @order.update(order_params)
+        format.html { redirect_to order_url(@order), notice: "Order was successfully updated." }
+        format.json { render :show, status: :ok, location: @order }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @order.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
-  # Confirmation page after successful order payment.
-  def confirm
-    # Implement logic for order confirmation here
+  # DELETE /orders/1 or /orders/1.json
+  def destroy
+    @order.destroy
+
+    respond_to do |format|
+      format.html { redirect_to orders_url, notice: "Order was successfully destroyed." }
+      format.json { head :no_content }
+    end
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_order
+      @order = Order.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def order_params
+      params.require(:order).permit(:name, :address, :email, :pay_type)
+    end
 end
