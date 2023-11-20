8a9079f6630b8115b0cd032a84902c006c2285a2
John <kiokoj81@gmail.com>
2023-11-20 22:10:29 +0300

    Create r/ships with line item

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,10 @@
 class Order < ApplicationRecord
+  has_many :line_items, dependent: :destroy
+ 
   enum pay_type: {
-    "Check" => 0,
+    "Cash on Delivery" => 0,
     "Credit card" => 1,
-    "Purchase order" => 2
+    "M-pesa" => 2
   }
 
   validates :name, :address, :email, presence: true
